14fb661d526d361e102f0e10d2b257349e278d58
walaura <hi@laura.monster>
2026-05-29 20:22:06 +0100

    add button component

---
+++ app/components/ui/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <NuxtLink v-if="isLink" :to="to" :class="buttonClasses" v-bind="$attrs">
+    <slot></slot>
+  </NuxtLink>
+  <button v-else="isLink" :class="buttonClasses" v-bind="$attrs" :type="buttonType">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  type: "primary" | "secondary" | "accent";
+  isSubmit?: boolean;
+  to?: string;
+}>();
+
+const buttonType = computed(() => (props.isSubmit ? "submit" : "button"));
+const isLink = computed(() => !!props.to);
+
+const buttonClasses = computed(() => {
+  return ["btn", props.type === "primary" ? "btn-primary" : props.type === "accent" ? "btn-accent" : "btn-secondary"];
+});
+</script>
