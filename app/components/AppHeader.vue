f0813bac602daa1cf0cec53bd2d8622588a4ef2e
walaura <hi@laura.monster>
2026-06-03 16:06:55 +0100

    lints

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -5,13 +5,13 @@
     <h1 class="mb-8 text-xl font-bold ml-4">Affirm</h1>
     <ul class="menu menu-lg w-full p-0">
       <li>
-        <NuxtLink to="/" class="link link-hover" activeClass="menu-active">Home</NuxtLink>
+        <NuxtLink to="/" class="link link-hover" active-class="menu-active">Home</NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/about" class="link link-hover" activeClass="menu-active">About</NuxtLink>
+        <NuxtLink to="/about" class="link link-hover" active-class="menu-active">About</NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/list" class="link link-hover" activeClass="menu-active">List</NuxtLink>
+        <NuxtLink to="/list" class="link link-hover" active-class="menu-active">List</NuxtLink>
       </li>
     </ul>
   </nav>
