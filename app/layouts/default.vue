4b49b3b7813145102d4a76c04390abd35b06813c
walaura <hi@laura.monster>
2026-06-03 16:15:11 +0100

    fix width

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex min-h-screen flex-row justify-center">
-    <div class="flex min-h-screen flex-column p-12 gap-8">
+    <div class="flex min-h-screen flex-column p-12 gap-8 max-w-240 w-full">
       <AppHeader class="flex-0 min-w-40" />
-      <main class="flex-1 max-w-3xl">
+      <main class="flex-1">
         <slot />
         <AppFooter />
       </main>
