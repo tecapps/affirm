736fd64852ed86448645e5931a0599da8ba8222e
walaura <hi@laura.monster>
2026-06-03 16:02:26 +0100

    ipad-style nav

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer footer-center bg-base-200 p-4 text-base-content">
+  <footer class="footer mt-12 p-4 text-base-content/80">
     <aside>
       <p>&copy; {{ new Date().getFullYear() }} TEC. Built with Nuxt 4 + DaisyUI.</p>
     </aside>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="flex min-h-screen flex-col">
-    <AppHeader />
-    <main class="flex-1 container mx-auto p-6">
-      <slot />
-    </main>
-    <AppFooter />
+  <div class="flex min-h-screen flex-row justify-center">
+    <div class="flex min-h-screen flex-column p-12 gap-8">
+      <AppHeader class="flex-0 min-w-40" />
+      <main class="flex-1 max-w-3xl">
+        <slot />
+        <AppFooter />
+      </main>
+    </div>
   </div>
 </template>
