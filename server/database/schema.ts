7ddd1740b954e1a5d319b0b204a6d61e2b4eadd4
Selene Posada <selene.s.posada@gmail.com>
2026-06-02 21:26:52 -0700

    finished updating user table, added foreign key to businesses table'

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -3,16 +3,25 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   email: text("email").notNull().unique(),
+  username: text("username").notNull().unique(),
   name: text("name").notNull(),
-  password: text("password"), // TODO: make this not null in prod
+  password: text("password").notNull(),
+  businessRepresentative: integer("businessRepresentative", { mode: "boolean" }).default(false),
+  business: integer("businessRepresentative")
+    .default(0)
+    .references(() => businesses.id),
+  verified: integer("verified", { mode: "boolean" }).default(false),
   createdAt: integer("created_at", { mode: "timestamp" })
     .notNull()
     .$defaultFn(() => new Date()),
   updatedAt: integer("updated_at", { mode: "timestamp" })
     .notNull()
-    .$defaultFn(() => new Date()),
+    .$defaultFn(() => new Date())
+    .$onUpdateFn(() => new Date()),
 });
-export const businesses = sqliteTable("businesses", {});
+export const businesses = sqliteTable("businesses", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+});
 
 export const tags = sqliteTable("tags", {});
 
