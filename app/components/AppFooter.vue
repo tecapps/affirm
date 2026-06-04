04fe720574a6ed05296b9a220fc087be1422edcc
walaura <hi@laura.monster>
2026-06-04 11:06:39 +0100

    some morning fixes

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer mt-12 p-4 text-base-content/80">
+  <footer class="mt-12 p-4 text-base-content/60 text-sm text-center items-center">
     <aside>
       <p>&copy; {{ new Date().getFullYear() }} TEC. Built with Nuxt 4 + DaisyUI.</p>
     </aside>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex min-h-screen flex-row justify-center">
-    <div class="flex min-h-screen flex-column p-12 gap-8 max-w-240 w-full">
-      <AppHeader class="flex-0 min-w-40" />
-      <main class="flex-1">
+    <div class="flex min-h-screen flex-column gap-6 max-w-240 w-full">
+      <AppHeader class="flex-0 min-w-40 sticky top-0 h-fit py-12" />
+      <main class="flex-1 py-12">
         <slot />
         <AppFooter />
       </main>
